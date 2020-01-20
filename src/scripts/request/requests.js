69fbdb2aa91ef694416b91672a6b75a21d00e7a9
WebInfUa <web.inf.ua@gmail.com>
2020-01-20 09:32:23 +0200

    Connect themify icons

--- src/scripts/request/requests.js
+++ src/scripts/request/requests.js
@@ -11,7 +11,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://geekhub-frontend-js-9.herokuapp.com/api/users/", requestOptions)
+await fetch("https://geekhub-frontend-js-9.herokuapp.com/api/users/", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
